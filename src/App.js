781873711d8c81cde9360018118c18188002cc2c
Ayush <ayuofficial11310@gmail.com>
2025-08-13 22:02:02 +0530

    Stating Project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-red-900">
+      <h1 className="text-4xl font-bold">Welcome to My YouTube Clone</h1>
+    </div>
+  );
+}
+
+export default App;
