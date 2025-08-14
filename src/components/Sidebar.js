c055512272784a3d73b666b7d9aef74d88a0c299
Ayush <ayuofficial11310@gmail.com>
2025-08-14 16:45:49 +0530

    Watch Page using UseParam completed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
     const isMenuOpen = useSelector(store => store.app.isMenuOpen)
 
     // Early return pattern 
-    // if(!isMenuOpen) return null;
+    if(!isMenuOpen) return null;
 
-    return isMenuOpen ? null : (
+    return  (
         <div className='p-5 shadow-lg w-48'>
             <ul>
-                <li className="text-lg">Homes</li>
-                <li className="text-lg">Shorts</li>
-                <li className="text-lg">Lives</li>
+                <li className="text-lg"> <Link to={"/"}>Homes</Link></li>
+                <li className="text-lg"> <Link to={"/shorts"}>Shorts</Link></li>
+                <li className="text-lg"> <Link to={"/live"}>Lives</Link></li>
             </ul>
             <h2 className="font-bold pt-5">Subscriptions</h2>
             <ul>
-                <li className="text-lg">Music</li>
-                <li className="text-lg">Trending</li>
-                <li className="text-lg">Sports</li>
+                <li className="text-lg"> <Link to={"/music"}>Music</Link></li>
+                <li className="text-lg"> <Link to={"/trending"}>Trending</Link></li>
+                <li className="text-lg"> <Link to={"/sports"}>Sports</Link></li>
             </ul>
             <h2 className="font-bold pt-5">Subscriptions</h2>
             <ul>
-                <li className="text-lg">Music</li>
-                <li className="text-lg">Trending</li>
-                <li className="text-lg">Sports</li>
+                <li className="text-lg"> <Link to={"/music"}>Music</Link></li>
+                <li className="text-lg"> <Link to={"/trending"}>Trending</Link></li>
+                <li className="text-lg"> <Link to={"/sports"}>Sports</Link></li>
             </ul>
         </div>
     )
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { YOUTUBE_VIDEOS_API } from './../utils/constants';
 import VideoCard from './VideoCard';
+import { Link } from 'react-router-dom';
 
 const VideoContainer = () => {
     const [videos, setVideos] = useState([]);
@@ -19,7 +20,9 @@ const VideoContainer = () => {
     return (
         <div className='flex flex-wrap justify-center'>
             {videos?.map((video) => (
-                <VideoCard key={video.id} info={video} />
+                <Link key={video.id} to={"/watch?v=" + video.id}>
+                    <VideoCard key={video.id} info={video} />
+                </Link>
             ))}
         </div>
     )
